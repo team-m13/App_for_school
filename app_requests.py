3098aad8d21296e644b326f0fc4d58ae4c53a8a6
team-m13 <75898962+team-m13@users.noreply.github.com>
2021-02-24 18:24:20 +0300

    Update app_requests.py

--- app_requests.py
+++ app_requests.py
@@ -1,3 +1,18 @@
+import requests
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from functools import partial
+
+from kivy.config import Config
+Config.set('graphics', 'resizable', 0)
+
+from kivy.core.window import Window
+Window.size = (400, 600)
+
 class MyApp(App):
 
 	def build(self):
